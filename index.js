b0145c502ac462dc78d61a47861fe588e2581a1b
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-02-19 23:15:14 +0500

    done

---
+++ index.js
@@ -0,0 +1,55 @@
+// Calculate Total Cost
+// Write a function called calculateTotalCost that takes two arguments: price (the price of an item) and quantity (the quantity of the item purchased). The function should calculate and return the total cost by multiplying the price and quantity. Test the function with different price and quantity values.
+// solution 
+//function calculateTotalCost (price,quantity){
+//let totalCost = price * quantity;
+//alert(totalCost);
+//}
+//calculateTotalCost (20,23);
+//calculateTotalCost (9,15);
+//calculateTotalCost (12,63);
+//calculateTotalCost (16,27);
+//calculateTotalCost (14,14);
+//calculateTotalCost (6,89);
+
+// Calculate Area of Rectangle
+// Develop a function called calculateArea that accepts two arguments: length and width of a rectangle. The function should calculate and return the area of the rectangle using the formula: area = length * width. Test the function with various length and width values.
+
+// solution
+ //function calculateArea(length,width) {
+ //   let area = length * width;
+ //   alert(area);
+ //}
+ //calculateArea(12,9);
+ //calculateArea(104,80);
+ //calculateArea(22,15);
+ //calculateArea(80,65);
+
+ //Calculate BMI (Body Mass Index) 
+ //Write a function called calculateBMI that takes two arguments: weight (in kilograms) and height (in meters). The function should calculate and return the BMI using the formula: BMI = weight / (height * height). Test the function with different weight and height values.
+
+ // solution
+ // 
+//function calculateBMI(weight,height) {
+//let = BMI = weight / (height * height);
+//alert (BMI);
+//}
+//calculateBMI(50,1.8);
+//calculateBMI(65,2);
+//calculateBMI(79,2);
+//calculateBMI(45,1.5);
+
+//Calculate Area of Triangle
+
+//Develop a function named calculateTriangleArea that accepts three arguments: base, height, and side. The function should calculate and return the area of a triangle using the formula: area = (base * height) / 2. Test the function with various base, height, and side values
+
+// SOLUTION 
+function calculateTriangleArea (base, height, side) {
+    let area = (base * height) / 2;
+    alert(area);
+    
+}
+calculateTriangleArea(5,7);
+calculateTriangleArea(8,12);
+calculateTriangleArea(3,5);
+calculateTriangleArea(10,12);
